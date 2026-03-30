2ffecf115d5c5a39c66c2c9c2a47474a4882da48
Keanujii <62798049+Keanuji@users.noreply.github.com>
2026-03-31 00:34:56 +0200

    feat: add mysphere.sphere.js

---
+++ mysphere.sphere.js
@@ -0,0 +1,20 @@
+/* jshint esversion:11 */
+(function(){
+'use strict';
+window.YM_S = window.YM_S || {};
+
+window.YM_S['mysphere.sphere.js'] = {
+  name: 'MySphere',
+  icon: '⬡',
+  category: 'Other',
+  description: 'My custom sphereoihoihohi',
+  emit: [], receive: [],
+
+  activate(ctx) {},
+  deactivate() {},
+
+  renderPanel(container) {
+    container.innerHTML = '<div style="padding:16px">Hello!</div>';
+  }
+};
+})();
