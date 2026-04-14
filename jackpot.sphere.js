b6739b8fd214f57a451231b025ae5f3e93ae1d4e
JdC <theodoreyong9@gmail.com>
2026-04-14 22:22:42 +0200

    Update jackpot.sphere.js

--- jackpot.sphere.js
+++ jackpot.sphere.js
@@ -815,7 +815,7 @@
   }
 
   /* ─── REGISTRATION ────────────────────────────────────── */
-  window.YM_S['striga.sphere.js'] = {
+  window.YM_S['jackpot.sphere.js'] = {
     name:        'Jackpot',
     icon:        '🎰',
     category:    'Finance',
